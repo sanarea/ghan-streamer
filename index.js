150c211a58ffc65f310187da8338c26d84bd1763
user <>
2019-06-17 14:48:13 +0900

    sfd

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-
 require('dotenv-safe').config();
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);;
 const mjpeg = require('./routes/mjpeg');
+app.use('/', express.static('www'));
 app.use('/mjpeg', mjpeg);
+
 server.listen(3000);
