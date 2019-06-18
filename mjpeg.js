8dba0a3df771e252dd40dcffe78d121bbe216558
user <>
2019-06-18 11:15:12 +0900

    vc

--- mjpeg.js
+++ mjpeg.js
@@ -1,83 +1,16 @@
 const spawn = require('child_process').spawn;
+let param = require('.env/ffmpeg');
 const {
     Writable
 } = require('stream');
 
-
-var params1 = [
-    ' -f', 'avfoundation',
-    '-i', '0',
-    '-framerate', '30',
-    '-video_size', '640x480',
-
-    // '-vcodec', ' libx264',
-    // '-profile:v', ' main',
-    // '-g', '25',
-    // '-r', '25 ',
-    // '-b:v', '500k ',
-    // '-keyint_min', '250',
-    // '-strict', 'experimental',
-    // '-pix_fmt', ' yuv420p',
-    // '-movflags', 'empty_moov+default_base_moof',
-    // '-an ',
-    '-preset', 'ultrafast',
-    '-f', 'mpjpeg',
-    "pipe:1"
-];
-
-/**
-  cvlc --no-audio v4l2:///dev/video0 \
-    --v4l2-width 1920 \
-    --v4l2-height 1080 \
-    --v4l2-chroma MJPG \
-    --v4l2-hflip 1 \
-    --v4l2-vflip 1 \
-    --sout '#standard{access=http{mime=multipart/x-mixed-replace;boundary=--7b3cc56e5f51db803f790dad720ed50a},mux=mpjpeg,dst=:8554/}' -I dummy
-  
- */
-var params = [
-    '-framerate', process.env.FRAMTE_RATE, // 없으면 자동으로 설정됨...
-    '-video_size', process.env.VIDEO_SIZE, // 이미지 품질
-    // '-c', 'copy',
-    // '-format',
-    '-f', process.env.INPUT_DRIVER,
-    '-i', process.env.INPUT_DEVICE,
-    // '-profile:v', ' main',
-    // '-g', '25', //
-    // '-r', '25', //
-    //'-tune','zerolatency', // not support pi
-    '-b:v', '500k', 
-    // '-threads', 2,
-    '-q:v', '9', //품질... 1..2
-    // "-preset",
-    // "ultrafast",
-    // '-pix_fmt', 'yuyv422p',
-    '-strict', 'experimental',
-    "-f",
-    "mpjpeg",
-    "pipe:1"
-];
 class mjpeg {
     constructor(){
-        if(process.env.PIXEL_FORMAT) {
-
-        }
     }
     async ffmpeg() {
 
-        // var ffmpeg = require('child_process').spawn("ffmpeg", [
-        //     '-framerate', '25',
-        //     '-video_size', '640x480',
-        //     '-f', 'avfoundation',
-        //     '-i', '0',
-        //     "-preset",
-        //     "ultrafast",
-        //     "-f",
-        //     "mpjpeg",
-        //     "pipe:1"
-        // ]);
         
-        var ffmpeg = require('child_process').spawn("ffmpeg", params);
+        var ffmpeg = require('child_process').spawn("ffmpeg", param);
         let cnt = 0;
         ffmpeg.on('error', (error) => {
             console.log(error);
