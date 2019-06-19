cc7def47d45a0920134cac5c8341529be2d79239
user <>
2019-06-19 13:04:37 +0900

    vc

---
+++ env/ffmpeg.macos.js
@@ -0,0 +1,25 @@
+
+var params = [
+    '-framerate', 25, // 없으면 자동으로 설정됨...
+    '-video_size', "1280x720", // 이미지 품질
+    // '-c', 'copy',
+    // '-format',
+    '-f', 'avfoundation',
+    '-i', '0',
+    // '-profile:v', ' main',
+    // '-g', '25', //
+    // '-r', '25', //
+    //'-tune','zerolatency', // not support pi
+    '-b:v', '500k',
+    // '-threads', 2,
+    '-q:v', '9', //품질... 1..2
+    // "-preset",
+    // "ultrafast",
+    // '-pix_fmt', 'yuyv422p',
+    '-strict', 'experimental',
+    "-f",
+    "mpjpeg",
+    "pipe:1"
+];
+
+module.exports = params;
