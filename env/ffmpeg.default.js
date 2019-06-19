d5eef5fbdc584a70d24ba94c0c88612799a915c8
user <>
2019-06-19 13:03:38 +0900

    sfd

--- env/ffmpeg.default.js
+++ env/ffmpeg.default.js
@@ -1,41 +1,11 @@
-var params1 = [
-    ' -f', 'avfoundation',
-    '-i', '0',
-    '-framerate', '30',
-    '-video_size', '640x480',
 
-    // '-vcodec', ' libx264',
-    // '-profile:v', ' main',
-    // '-g', '25',
-    // '-r', '25 ',
-    // '-b:v', '500k ',
-    // '-keyint_min', '250',
-    // '-strict', 'experimental',
-    // '-pix_fmt', ' yuv420p',
-    // '-movflags', 'empty_moov+default_base_moof',
-    // '-an ',
-    '-preset', 'ultrafast',
-    '-f', 'mpjpeg',
-    "pipe:1"
-];
-
-/**
-  cvlc --no-audio v4l2:///dev/video0 \
-    --v4l2-width 1920 \
-    --v4l2-height 1080 \
-    --v4l2-chroma MJPG \
-    --v4l2-hflip 1 \
-    --v4l2-vflip 1 \
-    --sout '#standard{access=http{mime=multipart/x-mixed-replace;boundary=--7b3cc56e5f51db803f790dad720ed50a},mux=mpjpeg,dst=:8554/}' -I dummy
-  
- */
 var params = [
-    '-framerate', process.env.FRAMTE_RATE, // 없으면 자동으로 설정됨...
-    '-video_size', process.env.VIDEO_SIZE, // 이미지 품질
+    '-framerate', 25, // 없으면 자동으로 설정됨...
+    '-video_size', "1280x720", // 이미지 품질
     // '-c', 'copy',
     // '-format',
-    '-f', process.env.INPUT_DRIVER,
-    '-i', process.env.INPUT_DEVICE,
+    '-f', 'avfoundation',
+    '-i', '0',
     // '-profile:v', ' main',
     // '-g', '25', //
     // '-r', '25', //
