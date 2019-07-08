6f109b50f30ff739125dbafb3bc04e68fc446950
user <>
2019-07-08 19:34:34 +0900

    codec copy

---
+++ env/ffmpeg.pi.codec.js
@@ -0,0 +1,12 @@
+var params = [
+    '-r', 15,
+    '-f', 'v4l2',
+    '-input_format', 'mjpeg',
+    '-i', '/dev/video0',
+    '-c:v', 'copy', // 순서가 중요하다...
+    "-f",
+    "mpjpeg",
+    "pipe:1"
+];
+
+module.exports = params;
